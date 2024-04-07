dbbd5ed6609a9f85a3e2c9796b38771b419a65f9
Kiril Yanev <kl_yanev@abv.bg>
2024-04-07 22:18:42 +0300

    fix: add render function to template.js

--- 01.intro/server-demo/controllers/aboutController.js
+++ 01.intro/server-demo/controllers/aboutController.js
@@ -1,13 +1,8 @@
-const { layout } = require("../util/template.js");
+const { layout, loadTemplate } = require("../util/template.js");
 
-
-const html = `
-    <div>
-        <h1>About Us!</h1>
-        <p>About page</p>
-    </div>`;
 
 module.exports = async (req, res) => {
-    res.write(await layout(html, 'About'));
+    const aboutPage = await loadTemplate('about');
+    res.write(await layout(aboutPage, 'About'));
     res.end();
 };
--- 01.intro/server-demo/util/template.js
+++ 01.intro/server-demo/util/template.js
@@ -9,12 +9,23 @@ async function loadTemplate(name) {
     }
 }
 
-async function layout(html, title = 'Welcome') {
-    const result = await loadTemplate('layout');
-    return result.replace('{{title}}', title).replace('{{body}}', html);
+async function layout(body, title = 'Welcome') {
+    return render('layout', { title, body });
+}
+
+async function render(name, context) {
+    let result = await loadTemplate(name);
+    const props = Object.keys(context);
+
+    for (let prop of props) {
+        result = result.replace(new RegExp(`{{${prop}}}`, 'g'), context[prop]);
+    }
+
+    return result;
 }
 
 module.exports = {
     loadTemplate,
-    layout
+    layout,
+    render
 };
